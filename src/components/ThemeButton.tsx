2d5d80ad0653f6132159c781b7d4b46ea6459aa2
JoleneKearse <pharaohnutz@gmail.com>
2025-11-28 12:07:47 -0500

    chore: finish light dark mode toggle

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -9,8 +9,21 @@ type ThemeProps = {
 };
 
 const ThemeButton = ({ theme, setTheme }: ThemeProps) => {
-  const handleThemeToggle = () =>
-    setTheme(theme === "light" ? "dark" : "light");
+  const handleThemeToggle = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+
+      if (newTheme === "dark") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
+
+      return newTheme;
+    });
+  };
 
   return (
     <button
@@ -20,9 +33,9 @@ const ThemeButton = ({ theme, setTheme }: ThemeProps) => {
       className="w-7 rounded-full hover:cursor-pointer"
     >
       {theme === "light" ? (
-        <img src={Sun} alt="Light Theme" className="mr-4" />
+        <img src={Moon} alt="Switch to dark theme" className="mr-4" />
       ) : (
-        <img src={Moon} alt="Dark Theme" className="mr-4" />
+        <img src={Sun} alt="Switch to light theme" className="mr-4" />
       )}
     </button>
   );
